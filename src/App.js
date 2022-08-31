035b5159423e75d820e8b0301beeec061c69c357
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-08-30 23:42:06 -0700

    move state handling into top component, change component from classes to functions

--- src/App.js
+++ src/App.js
@@ -9,8 +9,11 @@ class QuotePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pageColor: randomColor({luminosity: "dark"})
+      pageColor: randomColor({luminosity: "dark"}),
+      content: "",
+      author: ""
     }
+    this.handleClick = this.handleClick.bind(this);
     this.changeColor = this.changeColor.bind(this);
   }
 
@@ -20,11 +23,24 @@ class QuotePage extends React.Component {
     });
   }
 
-  componentDidMount () {
-      const script = document.createElement("script");
-      script.src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
-      script.async = true;
-      document.body.appendChild(script);
+  componentDidMount() {
+    this.updateQuote()
+  }
+
+  updateQuote() {
+    return (
+      fetch('https://api.quotable.io/random')
+        .then((res) => res.json())
+        .then((data) => this.setState({
+            content: data.content,
+            author: data.author
+          }))
+    );
+  }
+
+  handleClick() {
+    this.updateQuote()
+      .then(this.changeColor());
   }
 
   render() {
@@ -40,116 +56,75 @@ class QuotePage extends React.Component {
       <div style={pageStyle}>
         <QuoteBox
           background={this.state.pageColor}
-          colorAction={this.changeColor}
+          content={this.state.content}
+          author={this.state.author}
+          handleClick={this.handleClick}
         />
       </div>
     )
   }
 }
 
-class QuoteBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: "",
-      author: "",
-      update: true
-    };
-    this.handleClick = this.handleClick.bind(this);
+function QuoteBox (props) {
+  if (props.content === "") {
+    return <div />
   }
 
-  shouldComponentUpdate (nextProps, nextState) {
-   return nextState.update;
+  const quoteBoxStyle = {
+    width: "75vw",
+    maxWidth: 500,
+    margin: "auto",
+    padding: "5vw",
+    backgroundColor: "white",
+    borderRadius: 5
   }
 
-  componentDidMount() {
-    this.updateQuote()
-  }
-
-  componentDidUpdate() {
-    this.setState({ update: false })
-  }
-
-  updateQuote() {
-    return (
-      fetch('https://api.quotable.io/random')
-        .then((res) => res.json())
-        .then((data) => this.setState({
-            content: data.content,
-            author: data.author
-          }))
-    );
-  }
-
-  handleClick() {
-    this.setState({ update: true });
-    this.updateQuote()
-      .then(this.props.colorAction());
-  }
-
-  render() {
-    if (this.state.content === "") {
-      return <div />
-    }
-
-    const quoteBoxStyle = {
-      width: "75vw",
-      maxWidth: 500,
-      margin: "auto",
-      padding: "5vw",
-      backgroundColor: "white",
-      borderRadius: 5
-    }
-
-    return (
-      <div id="quote-box" style={quoteBoxStyle}>
-        <Quote quote={this.state.content}/>
-        <Author author={this.state.author}/>
-        <ButtonGroup
-          background={this.props.background}
-          action={this.handleClick}
-          quote={this.state.content}
-          author={this.state.author}
-        />
-      </div>
-    );
-  }
+  return (
+    <div id="quote-box" style={quoteBoxStyle}>
+      <Quote quote={props.content}/>
+      <Author author={props.author}/>
+      <ButtonGroup
+        background={props.background}
+        action={props.handleClick}
+        quote={props.content}
+        author={props.author}
+      />
+    </div>
+  );
 };
 
-class ButtonGroup extends React.Component {
-  render() {
-    const buttonGroupStyle = {
-      display: "flex",
-      justifyContent: "space-between"
-    }
-
-    return (
-      <div style={buttonGroupStyle}>
-        <div id="linkBoxes">
-          <Button
-            background={this.props.background}
-            buttonIcon= {<FaTwitter />}
-            aElement={true}
-            href={GenerateTweet(this.props.quote, this.props.author)}
-          />
-          <Button
-            background={this.props.background}
-            buttonIcon= {<FaTumblr />}
-            aElement={true}
-            href={GenerateTumblr(this.props.quote, this.props.author)}
-          />
-        </div>
-        <div id="actionBox">
-          <Button
-            background={this.props.background}
-            id="new-quote"
-            buttonIcon={"New Quote"}
-            getQuote={this.props.action}
-          />
-        </div>
-       </div>
-    )
+function ButtonGroup (props) {
+  const buttonGroupStyle = {
+    display: "flex",
+    justifyContent: "space-between"
   }
+
+  return (
+    <div style={buttonGroupStyle}>
+      <div id="linkBoxes">
+        <Button
+          background={props.background}
+          buttonIcon= {<FaTwitter />}
+          aElement={true}
+          href={GenerateTweet(props.quote, props.author)}
+        />
+        <Button
+          background={props.background}
+          buttonIcon= {<FaTumblr />}
+          aElement={true}
+          href={GenerateTumblr(props.quote, props.author)}
+        />
+      </div>
+      <div id="actionBox">
+        <Button
+          background={props.background}
+          id="new-quote"
+          buttonIcon={"New Quote"}
+          getQuote={props.action}
+        />
+      </div>
+     </div>
+  )
 }
 
 const GenerateTweet = (quote, author) => {
@@ -163,43 +138,41 @@ const GenerateTumblr = (quote, author) => {
   return [tumblr_base, author, "&content=", quote, tumblr_end].join("")
 }
 
-class Button extends React.Component {
-  render() {
-    const buttonStyle = {
-      backgroundColor: this.props.background,
-      color: "white",
-      border: "none",
-      borderRadius: 2,
-      height: 42,
-      minWidth: 42,
-      margin: 2,
-      fontSize: ".9em"
-    }
+function Button (props) {
+  const buttonStyle = {
+    backgroundColor: props.background,
+    color: "white",
+    border: "none",
+    borderRadius: 2,
+    height: 42,
+    minWidth: 42,
+    margin: 2,
+    fontSize: ".9em"
+  }
 
-    if (this.props.aElement === true) {
-      return (
-          <button style={buttonStyle}>
-            <a
-              id="tweet-quote"
-              href={this.props.href}
-              target="_blank"
-              rel="noreferrer"
-              style={{color:"white"}}
-            >
-              {this.props.buttonIcon}
-            </a>
-          </button>
-        );
-    } else {
-      return (
-          <button
-            style={buttonStyle}
-            onClick={this.props.getQuote}
+  if (props.aElement === true) {
+    return (
+        <button style={buttonStyle}>
+          <a
+            id="tweet-quote"
+            href={props.href}
+            target="_blank"
+            rel="noreferrer"
+            style={{color:"white"}}
           >
-            {this.props.buttonIcon}
-          </button>
-        );
-    }
+            {props.buttonIcon}
+          </a>
+        </button>
+      );
+  } else {
+    return (
+        <button
+          style={buttonStyle}
+          onClick={props.getQuote}
+        >
+          {props.buttonIcon}
+        </button>
+      );
   }
 }
 
